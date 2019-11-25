94ea327d7cf75adfcaaca49436aa3a501ece91ce
Shoya Chinen <e195743@ie.u-ryukyu.ac.jp>
2019-11-25 14:49:02 +0900

    second

--- src/main/jp/ac/uryukyu/ie/e195743/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195743/Enemy.java
@@ -53,6 +53,7 @@ public class Enemy {
         }
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
+        System.out.println("aa");
     }
 
     /**
